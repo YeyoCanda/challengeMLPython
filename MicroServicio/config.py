407be78e8e894f4f2813b05495dfe3b33bb33a77
YeyoCanda <yeyocanda88@gmail.com>
2022-05-27 21:38:44 -0500

    remove mongo url

--- MicroServicio/config.py
+++ MicroServicio/config.py
@@ -5,4 +5,4 @@ URL_ML_CATEGORY = "https://api.mercadolibre.com/categories/"
 URL_ML_CURRENCY = "https://api.mercadolibre.com/currencies/"
 URL_ML_SELLER = "https://api.mercadolibre.com//users/"
 NUM_OF_THREADS = 100
-URI_MONGO = "mongodb://dbcosmosmongo:n7TDhqiOB3MCgfovdmvWFUQDhTfldHrBAmlkEnj3DwyW0BrQhJLXrCd14DfAs1ql1KSbC3u7L2afTVNWm3hBFA==@dbcosmosmongo.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@dbcosmosmongo@"
+URI_MONGO = "mongodb://dbcosmosmongo" #por seguridad quito la url completa
